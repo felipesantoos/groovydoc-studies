394716d86aa504c2216f512478e09913320acd62
felipesantoos <felipedssantos811@gmail.com>
2022-05-20 13:33:40 -0300

    Add method with keyword name example

--- 005_Shebang.groovy
+++ 005_Shebang.groovy
@@ -5,4 +5,5 @@ println "Hello from the shebang line"
  * You'll need to set the permissions of this file.
  * In the Linux you can do it with the `chmod 777 005_Shebang.groovy`
  * command, for example.
+ * Once you have the execution permission, you can run `./005_Shebang.groovy`.
  */
---
+++ 006_MethodWithReservedWordName.groovy
@@ -0,0 +1,4 @@
+// Reserved work as method name.
+def "abstract"() { true }
+// You need to call this method with "this." to the left.
+println this.abstract()
